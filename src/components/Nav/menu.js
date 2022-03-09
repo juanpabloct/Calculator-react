fb6aabdcc21b4e4d4b415e2ccf9b10dce9605c8f
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-03-09 08:31:25 -0500

    Se han quitado cosas que eran necesarias en el menu

--- src/components/Nav/menu.js
+++ src/components/Nav/menu.js
@@ -1,35 +1,12 @@
 import React from "react";
-import clsx from "clsx";
-import { useState } from "react";
 import "./menu.css";
+import logoOficial from "../../assets/img/logoOficial.svg";
 
 function Menu() {
-  const arrayGeneros = [
-    "Accion",
-    "Terror",
-    "Ciencia Ficcion",
-    "Aventura",
-    "Comedia",
-    "Fantasia",
-  ];
-  const [hover, setShow] = useState(false);
-  const generos = arrayGeneros.map((value) => <span key={value}>{value}</span>);
-  console.log(hover);
   return (
     <>
       <div className="menu">
-        <div>Home</div>
-        <div
-          className="c-peliculas"
-          onMouseEnter={() => setShow(true)}
-          onMouseLeave={() => setShow(false)}
-        >
-          Peliculas
-          <div className={clsx("peliculas", hover ? "show" : "")}>
-            {generos}
-          </div>
-        </div>
-        <div>Series</div>
+        <img src={logoOficial} alt="Calculator juan" />
       </div>
     </>
   );
