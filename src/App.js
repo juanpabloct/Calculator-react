3661a09497133dd4092747e0c5413e6298307e44
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-03-09 08:26:01 -0500

    Se ha quitado una botonera de prueba

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import { useState } from "react";
-import Button from "./components/Button";
 import Menu from "./components/Nav/menu";
 import Calculadora from "./components/calculadora/calculadora";
 
 function App() {
-  const [contador, setContador] = useState(0);
   return (
     <>
       <Menu />
-      <div>
-        <Button name="Sumar" onClick={() => setContador(contador + 1)} />
-        <Button name="Restar" onClick={() => setContador(contador - 1)} />
-      </div>
-      <div>{contador}</div>
       <Calculadora />
     </>
   );
